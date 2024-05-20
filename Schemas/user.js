69d509bfeb87ce6f040ff6ef6c1f6e0ff9a60a43
SammarJuneja <sammarkumar345@gmail.com>
2024-05-21 01:11:32 +0530

    Fixed user.js schema

--- Schemas/user.js
+++ Schemas/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
@@ -35,4 +35,4 @@ const userSchema = new mongoose.schema({
 
 const user = mongoose.model("User", userSchema);
 
-module.exports = User;
+module.exports = user;
