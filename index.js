9833063ee030344213d8ea6a07ff02e2977bf4ba
SammarJuneja <sammarkumar345@gmail.com>
2024-05-21 17:14:10 +0530

    Changed login endpoint o POST from GET

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-app.get("/login", async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   
   if (!email || !password) {
