fd3b98b930e3a09cb25bf97991c7e46c07a18071
KoolMohPC <justmohammad66@gmail.com>
2024-05-26 18:56:40 +0300

    I forgot the changes i made yesterday and too lazy to review everything

--- Routes/userRoutes/index.js
+++ Routes/userRoutes/index.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
 const config = require('../../config.js');
+const authenticateJWT = require('../../Middleware/Authorization.js');
+const User = require('../../Database/Models/userModel.js');
 
 const router = Router();
 
-router.get('user', (req, res) => {
-  
+router.get('users', authenticateJWT, async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ users });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 })
 
 module.exports = router;
