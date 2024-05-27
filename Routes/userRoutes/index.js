6c363eef25019f89ad4e2aba0715d0f501231eca
SammarJuneja <sammarkumar345@gmail.com>
2024-05-27 14:38:10 +0530

    Added avatar

--- Routes/userRoutes/index.js
+++ Routes/userRoutes/index.js
@@ -1,10 +1,18 @@
-const { Router } = require('express');
-const config = require('../../config.js');
+const { Router } = require("express");
+const config = require("../../config.js");
+const authorization = require("../../Middleware/authorization.js");
+const User = require("../../Database/Models/userModel.js");
 
 const router = Router();
 
-router.get('user', (req, res) => {
+router.post(
+  "update-apperance",
+  [
+    body("username")
+  ],
+  authorization,
+  async (req, res) => {
   
-})
+});
 
 module.exports = router;
