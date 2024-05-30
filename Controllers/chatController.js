8f4ebc064a7bc190015e0f45b29e3f2e9236cae3
SammarJuneja <sammarkumar345@gmail.com>
2024-05-31 00:16:33 +0530

    Fixed error handling

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -3,18 +3,21 @@ const Chat = require('../Database/Models/chatModel.js');
 
 exports.startChat = async (req, res) => {
   try {
-    const { userid } = req.body;
-    const loggedUser = req.userId;
-    const chatGet = await Chat.find({ participants: [loggedUser, userid] });
-    //  idk what i did here
+    const { participants } = req.body;
+    const chatGet = await Chat.find({
+      participants
+    });
     if (chatGet.length === 2) {
       res.status(200).json({ chatGet });
     } else {
-      const newChat = new Chat({ participants });
+      const newChat = new Chat({
+        participants
+      });
       await newChat.save()
     }
     res.status(200).json({ newChat });
   } catch (error) {
-    res.status(500).json({ error: error });
+    console.log(error)
+    res.status(500).json({ error: "Internal server error" });
   }
 }
