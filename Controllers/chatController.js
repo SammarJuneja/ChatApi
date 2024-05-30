18765ac21d6270ab1b21f440467b9ad329d36be6
SammarJuneja <sammarkumar345@gmail.com>
2024-05-31 00:23:03 +0530

    Fixed startChat

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -3,19 +3,24 @@ const Chat = require('../Database/Models/chatModel.js');
 
 exports.startChat = async (req, res) => {
   try {
-    const { participants } = req.body;
+    const { userid } = req.body;
+    const loggedUser = req.userId
+    const users = [loggedUser, userid]
+    console.log(users)
     const chatGet = await Chat.find({
-      participants
+      $in: {
+        users
+      }
     });
     if (chatGet.length === 2) {
       res.status(200).json({ chatGet });
     } else {
       const newChat = new Chat({
-        participants
+        users
       });
       await newChat.save()
+      res.status(200).json({ newChat });
     }
-    res.status(200).json({ newChat });
   } catch (error) {
     console.log(error)
     res.status(500).json({ error: "Internal server error" });
