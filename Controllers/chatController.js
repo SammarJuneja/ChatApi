00097f8e142b650d422ab915eb1146cd26540fc0
SammarJuneja <sammarkumar345@gmail.com>
2024-05-30 16:13:37 +0530

    Fixed /startChat

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -3,16 +3,14 @@ const Chat = require('../Database/Models/chatModel.js');
 
 exports.startChat = async (req, res) => {
   try {
-    const { participants } = req.body;
-    const chatGet = await Chat.find({
-      participants
-    });
+    const { userid } = req.body;
+    const loggedUser = req.userId;
+    const chatGet = await Chat.find({ participants: [loggedUser, userid] });
+    //  idk what i did here
     if (chatGet.length === 2) {
       res.status(200).json({ chatGet });
     } else {
-      const newChat = new Chat({
-        participants
-      });
+      const newChat = new Chat({ participants });
       await newChat.save()
     }
     res.status(200).json({ newChat });
